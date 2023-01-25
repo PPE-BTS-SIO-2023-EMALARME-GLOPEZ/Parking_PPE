c438e0710dabb2ad5d8166208f943066f87c1a27
glopez <glopez-dev@proton.me>
2023-01-25 16:15:28 +0100

    Revert "Test sur les routes et les blades"
    This reverts commit 072a2ea351cd0828ae28b4b05acb3cfca3b837a0.

--- parking/routes/web.php
+++ parking/routes/web.php
@@ -14,11 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-//Route::get('/', function () {
-//return view('login');
-//});
-
-Route::redirect('/', 'login');
+Route::get('/', function () {
+    return view('welcome');
+});
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -30,4 +28,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__ . '/auth.php';
+require __DIR__.'/auth.php';
