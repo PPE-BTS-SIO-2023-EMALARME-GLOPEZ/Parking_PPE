b2b66efaed2c76ed2c1a33028e8cb7db7222187c
glopez <glopez-dev@proton.me>
2023-01-26 17:08:00 +0100

    Vue dashboard

--- parking/resources/views/dashboard.blade.php
+++ parking/resources/views/dashboard.blade.php
@@ -12,26 +12,28 @@
     <title>Parking</title>
 </head>
 
-<body>
-    <div class="h-screen w-screen sm:w-1/6  px-6 py-10 text-white bg-black flex flex-col fixed z-0">
+<body class="flex flex-row bg-gradient-to-t from-spanish-gray to-pale-silver">
 
-        <!-- Navbar logo -->
-        <div class="h-8 w-full text-center text-2xl lg:text-lg">
-            <i class=" fa-solid fa-car-side"></i>
+    <!-- Menu sidebar -->
+    <div class="h-screen w-screen sm:w-1/4 xl:w-1/6  px-6 py-10 text-black bg-gradient-to-t from-spanish-gray to-pale-silver flex flex-col z-10 absolute sm:static">
+
+        <!-- Sidebar logo -->
+        <div class="h-8 w-full text-center text-2xl sm:text-xl lg:text-2xl xl:text-xl">
+            <i class=" fa-solid fa-car-side text-middle-grey"></i>
             <span class="ml-1">Parking</span>
         </div>
 
         <!-- User profile card -->
         <div class="w-full h-fit my-11 py-3 flex flex-col justify-center align-middle items-center">
             <!-- Profile pic -->
-            <div class="w-16 h-16 mb-3 bg-blue-500 flex flex-col justify-end align-bottom items-end rounded-full">
+            <div class="w-16 h-16 mb-3 bg-white flex flex-col justify-end align-bottom items-end rounded-full">
                 <!-- Edit badge -->
                 <div class="flex flex-row justify-center items-center">
-                    <i class="fa-solid fa-pen w-fit h-fit text-xs bg-orange-600 rounded-full"></i>
+                    <i class="fa-solid fa-exclamation w-4 h-4 pt-px text-xs text-white bg-coquelicot text-center rounded-full"></i>
                 </div>
             </div>
             <!-- Profile name -->
-            <div class="text-lg lg:text-base text-white text-center">
+            <div class="text-lg lg:text-xl xl:text-lg text-center">
                 <!-- add blade user full name -->
                 <span class="">Lopez</span>
                 <span>Gabriel</span>
@@ -39,7 +41,7 @@
         </div>
 
         <!-- Nav links -->
-        <nav class="w-full h-fit xl:px-2 lg:text-sm text-base flex row xl:justify-start justify-center">
+        <nav class="w-full h-fit text-base lg:text-lg xl:text-base flex row justify-center">
             <ul class="text-center">
                 <li class="py-2">Dashboard</li>
                 <li class="py-2"><span>Folders</span></li>
@@ -55,6 +57,80 @@
                 <span>Déconnexion</span>
             </button>
         </div>
+    </div>
+
+    <!-- Dashboard -->
+    <div class="w-full h-screen text-clip grow flex flex-col bg-timberwolf sm:static">
+
+        <!-- Moitié supérieure -->
+        <div class="h-fill grow flex flex-col">
+
+            <!-- Barre supérieure -->
+            <div class="w-fill h-1/4 pt-10 mx-10 flex flex-row justify-between ">
+
+                <h3 class="text-xl">
+                    Disponibilité
+                    <span class="text-sm ml-1 p-1 h-fit text-white bg-black text-center rounded-full translate-y-1">32 places</span>
+                </h3>
+
+                <div class="w-fit flex flex-row justify-between align-middle">
+                    <button class="w-10 h-10 p-1 mr-3 text-base rounded-lg border-solid border-spanish-gray border-2 text-black shadow-sm">
+                        <i class="fa-regular fa-bell"></i>
+                    </button>
+                    <button class="w-10 h-10 p-1 mr-3 text-base rounded-lg border-solid border-spanish-gray border-2 text-black shadow-sm">
+                        <i class="fa-solid fa-arrow-right-from-bracket"></i>
+                    </button>
+                    <button class="w-fit h-10 px-2 text-base rounded-lg  text-white bg-middle-grey shadow-lg">
+                        Cliquez moi !
+                    </button>
+                </div>
+            </div>
+
+            <!-- Contenu partie supérieure -->
+            <div class="w-fill h-3/4 mt-11 mx-10">
+
+                <!-- Gros texte -->
+                <div class="w-5/12 flex flex-col">
+                    <span class="text-4xl mb-3 font-normal">
+                        Gestion des réservations
+                    </span>
+                    <span class="text-base">
+                        Vous ne pouvez avoir qu'une seule réservation active à la fois,
+                        cependant vous pouvez l'annuler a tout moment pour en faire une nouvelle
+                        l'attribution d'une place se fait en fonction des disponibilités.
+                    </span>
+                </div>
+
+
+                <!-- Carrousel -->
+                <div></div>
+            </div>
+
+        </div>
+
+        <!-- Moitié inférieure -->
+        <div class="h-1/2 my-10 w-full flex flex-row justify-around">
+
+            <!-- Reservation -->
+            <div class="w-4/12 ml-10 p-6 bg-white rounded-3xl flex flex-col drop-shadow-lg">
+                <div>
+                    <!-- Titre -->
+                    <h3>Reservation</h3>
+                    <!-- Bouton options -->
+                    <div>
+                        infos réservation
+                    </div>
+
+                </div>
+            </div>
+
+            <!-- Historique -->
+            <div class="w-8/12 mx-10 p-6 bg-white rounded-3xl flex flex-col drop-shadow-lg bg-gradient-to-bl from-spanish-gray to-pale-khaki-web f">
+                <h3>Historique</h3>
+            </div>
+
+        </div>
+
     </div>
 </body>
 
