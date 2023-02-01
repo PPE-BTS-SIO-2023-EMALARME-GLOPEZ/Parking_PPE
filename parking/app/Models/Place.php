ffb3166554a686bfca78cd8df67438cdab0f24f6
glopez <glopez-dev@proton.me>
2023-02-01 15:00:46 +0100

    Refactorisation du code lié aux reservations

--- parking/app/Models/Place.php
+++ parking/app/Models/Place.php
@@ -50,6 +50,6 @@ class Place extends Model
     private static function reattribuer(Place $place_libérée)
     {
         $reservation = ListeAttente::retirerPremier();
-        $reservation = Reservation::attribuerPlace($reservation, $place_libérée);
+        $reservation->attribuerPlace($reservation, $place_libérée);
     }
 }
