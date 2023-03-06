6200bb9110340d86d0c484daad68869fced07b7a
etihaine <78075407+etihaine@users.noreply.github.com>
2023-03-06 10:20:49 +0100

    est_valid

--- parking/database/seeders/DatabaseSeeder.php
+++ parking/database/seeders/DatabaseSeeder.php
@@ -17,5 +17,22 @@ class DatabaseSeeder extends Seeder
         \App\Models\User::factory(5)->create();
         \App\Models\Place::factory(3)->create();
         \App\Models\Parametre::factory(1)->create();
+<<<<<<< Updated upstream
+=======
+        $this->admin();
+    }
+
+    private function admin()
+    {
+        $admin = new User;
+        $admin->nom_utilisateur = ucfirst(env('ADMIN_NAME', 'admin'));
+        $admin->prenom_utilisateur = ucfirst(env('ADMIN_SURNAME', 'admin'));
+        $admin->username = env('ADMIN_USERNAME', 'admin');
+        $admin->password = Hash::make(env('ADMIN_PASSWORD', 'password'));
+        $admin->est_admin = true;
+        $admin->reservation_id = null;
+        $admin->est_valide = true;
+        $admin->save();
+>>>>>>> Stashed changes
     }
 }
